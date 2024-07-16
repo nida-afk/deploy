5b9bf72276bee64b34d3ba1099fa2414e51a78a6
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-16 09:37:02 +0000

    Tue, Jul 16, 2024, 3:07 PM +05:30

--- dep/hello/views.py
+++ dep/hello/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world")
+def index(request, name):
+    return HttpResponse("Hello "+ name)
 
