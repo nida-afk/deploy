4c6bbc0c4c02439724fa0f22ed357bde4541fa52
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-16 13:26:10 +0000

    Tue, Jul 16, 2024, 6:56 PM +05:30

--- dep/hello/views.py
+++ dep/hello/views.py
@@ -5,12 +5,13 @@ import datetime
 
 # Create your views here.
 def index(request, name):
-    now = datetime.now()
+    now = datetime.datetime.now()
     return render(request, "index.html", {
         "name": name.capitalize(),
         "msg": "Successful !",
         "h": now.hour ,
         "m": now.minute ,
-        "s" :now.second
+        "s" :now.second,
+        "if": now.day
     })
 
