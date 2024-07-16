1377e3d36f8131f3f7978979d4455f9c8521de57
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-16 13:20:25 +0000

    Tue, Jul 16, 2024, 6:50 PM +05:30

--- dep/hello/views.py
+++ dep/hello/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
-now = datetime.now()
+
 # Create your views here.
 def index(request, name):
+    now = datetime.now()
     return render(request, "index.html", {
         "name": name.capitalize(),
-         "msg": "Successful !",
+        "msg": "Successful !",
         "h": now.hour ,
         "m": now.minute ,
         "s" :now.second
