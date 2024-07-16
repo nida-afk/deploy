3290a8c949b05192f33c6250693ba2357b168f4e
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-16 09:48:58 +0000

    Tue, Jul 16, 2024, 3:18 PM +05:30

--- dep/hello/views.py
+++ dep/hello/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request, name):
-    return HttpResponse("Hello "+ name.capitalize())
+    return render(request, "index.html", {
+        "name": name.capitalize(),
+         "msg": "Successful !"
+    })
 
