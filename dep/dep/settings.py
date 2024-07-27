e01eccbcff5461df69760b92f19bfb05437c5eaa
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-27 12:12:38 +0000

    Sat, Jul 27, 2024, 5:42 PM +05:30

--- dep/dep/settings.py
+++ dep/dep/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
