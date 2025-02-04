65b34b9264a2e1a37047ec8a3a07e8ee4201088e
nida-afk <87021685+nida-afk@users.noreply.github.com>
2025-02-04 19:04:26 +0530

    Delete dep directory

--- dep/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path("", views.index, name = "index"),
-    path("tasks", views.tasks, name = "tasks"),
-    path("choose", views.choose, name = "choose"),
-]
